4149a14b02c5581f4770ba8ddc3d67bee7b02227
kai li <lk199151@gmail.com>
2020-07-07 20:41:08 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View style={styles.row}>
+      <Text>Universal React with Expodddd</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
