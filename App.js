95803548c039335d06f2ae1f4489194d3b5738e5
kai li <lk199151@gmail.com>
2020-07-09 21:07:55 -0400

    add stack nav

--- App.js
+++ App.js
@@ -5,34 +5,47 @@ import {Provider} from 'react-redux'
 import NewDeck from "./src/components/NewDeck";
 import Deck from "./src/components/Deck"
 import reducer from './src/reducers'
-import {createStackNavigator} from 'react-navigation';
 import {purple, white} from './src/utils/colors'
 import {FontAwesome, Ionicons} from '@expo/vector-icons'
 import {Constants} from 'expo'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
 
 
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 const TabBar = () => {
     return (
-        <NavigationContainer style={styles.row}>
+
             <Tab.Navigator>
                 <Tab.Screen name="Deck" component={Deck}/>
                 <Tab.Screen name="New Deck" component={NewDeck}/>
             </Tab.Navigator>
-        </NavigationContainer>
+
     )
+}
+
+const MyStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={TabBar} />
+    </Stack.Navigator>
+  );
 }
 
 export default function App() {
     return (
 
         <Provider store={createStore(reducer)}>
-            <TabBar/>
+            <NavigationContainer style={styles.row}>
+            <MyStack/>
+                {/*<TabBar/>*/}
+                {/*<Deck/>*/}
+            </NavigationContainer>
         </Provider>
 
 
